57ed803235d48c5c30fd8e6b671fad48514221f1
Sathvika <sathvikamuktha@gmail.com>
2024-05-03 17:01:28 -0400

    slideshow

--- itis3135/scripts/headerfooter.js
+++ itis3135/scripts/headerfooter.js
@@ -46,6 +46,13 @@ document.addEventListener("DOMContentLoaded", function() {
         menuItem.textContent = item.name;
         menuItem.href = item.url;
 
+        // Create and append the butterfly
+        const butterfly = document.createElement("img");
+        butterfly.src = "images/butterflyPNG.png";
+        favicon.alt = "butterfly";
+        butterfly.classList.add("butterfly");
+        menuItem.appendChild(butterfly)
+
         return menuItem;
     }
 
