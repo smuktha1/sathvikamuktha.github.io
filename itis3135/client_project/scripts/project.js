815dc339ca56a7a78f840e1436bdb1ca21098df6
Sathvika <sathvikamuktha@gmail.com>
2024-05-06 14:41:20 -0400

    2.41pm

--- itis3135/client_project/scripts/project.js
+++ itis3135/client_project/scripts/project.js
@@ -4,10 +4,4 @@ document.addEventListener('DOMContentLoaded', function() {
     currentTimeElement.textContent = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + 
                                     ' on ' + currentDate.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
-    document.getElementById('user-info').addEventListener('submit', function(event) {
-        event.preventDefault();
-        var name = document.getElementById('name').value;
-        var mood = document.getElementById('mood').value;
-        document.getElementById('greeting').textContent = "Hello " + name + "!! Sagacious Mermaid welcomes you!! We're glad you are doing " + mood + " today!";
-    });
 })
